376d070bb00337a16978b35aefc28301b40c3d49
Eric Kwong <erickwong618@yahoo.com>
2015-04-23 18:06:37 -0400

    fixed server listen

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use(express.static(__dirname + '/public'));
 // });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.listen( process.env.PORT || 3000, function () {
+    console.log('Running on 3000!');
 });
 
 module.exports = app;
